40972f991ce586efef13bf9abc841bcb859cd44b
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-21 20:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	var form = document.forms['form1'];
+
+    // Extract the first name and last name
+    var firstName = form['fname'].value;
+    var lastName = form['lname'].value;
+
+    // Show the full name in an alert
+    alert(firstName + ' ' + lastName);
 
 }
